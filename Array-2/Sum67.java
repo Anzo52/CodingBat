8567a5eaf3c492f071927188743b6c24c19d9c8f
Anzo52 <azollner2014@gmail.com>
2022-04-30 12:12:39 -0600

    Update Sum67.java

--- Array-2/Sum67.java
+++ Array-2/Sum67.java
@@ -2,14 +2,12 @@ public class Sum67 {
     public static int sum67(int[] nums) {
         // return sum except nums between "6" and "7"
         int sum = 0;
-        int i = 0;
-        while (i < nums.length) {
-            if (nums[i] == 6 && nums[i] != 7) {
-                i += 2;
-            } else {
-                sum += nums[i];
-                i++;
-            }
+        boolean not7 = false;
+        
+        for (int n : nums) {
+            if (n == 6) not7 = true;
+            if (!not7) sum += n;
+            if (not7 && n == 7) not7 = false;
         }
         return sum;
     }
