7a62323ef33b480feb985e02b2cb1f3efdbdcbed
Anzo52 <azollner2014@gmail.com>
2022-05-09 10:18:30 -0600

    Add has22

---
+++ Java/Array-2/Has22.java
@@ -0,0 +1,14 @@
+// print true if array has 2 followed by another 2
+
+public class Has22 {
+
+    public static boolean has22(int[] nums) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            if (nums[i] == 2 && nums[i+1] == 2) return true;
+        }
+
+        return false;
+    }
+}
